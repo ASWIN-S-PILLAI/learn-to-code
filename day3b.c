9e601120662174b528c9503abb2dc54988b5ebad
ASWIN-S-PILLAI <84024681+ASWIN-S-PILLAI@users.noreply.github.com>
2021-05-13 15:36:02 +0530

    Create day3b.c

---
+++ day3b.c
@@ -0,0 +1,15 @@
+//Write a C Program to convert Fahrenheit to Celsius.
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() { float a;
+            scanf("%f",&a);
+            printf("%0.2f",((a-32)/9)*5);
+
+        
+    return 0;
+}
