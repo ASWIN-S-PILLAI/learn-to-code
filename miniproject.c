1caa6c3a9ca7337cb35e7e02c56444b39a15fd16
ASWIN-S-PILLAI <84024681+ASWIN-S-PILLAI@users.noreply.github.com>
2021-06-11 22:13:13 +0530

    Create miniproject.c

---
+++ miniproject.c
@@ -0,0 +1,58 @@
+//MINI ATM
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+
+{float x,y; char a[1000]; int b,n;
+ printf("Enter name ");
+ gets(a);
+ 
+ printf("Enter Acc No ");
+ scanf("%d",&b);
+ 
+ printf("Enter Initial Ammount ");
+ scanf("%f",&x);
+ 
+ 
+ th:
+ 
+ printf("Select transaction \n");
+ printf("1.DEPOSIT\n2.WITHDRAWAL\n");
+ scanf("%d",&n);
+ 
+ switch(n)
+{case 1:{ printf("Enter Ammount\n");
+scanf("%f",&y);
+x=x+y;
+printf("Balance=%f",x);
+break;}
+
+case 2: {  printf("Enter Ammount\n");
+scanf("%f",&y);
+
+if(y>x)
+printf("Insufficient Acc Balance");
+
+else
+{x=x-y;
+printf("Balance=%f",x);
+
+}
+	break;
+}
+
+default: { printf("choose correct option");
+goto th;
+	break;
+}
+
+}
+ }
+
